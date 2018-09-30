e38400bbfa8a705351b1c2bb26bc352ffbc27804
erickhdz01 <43705709+erickhdz01@users.noreply.github.com>
2018-09-29 19:22:59 -0600

    django installed

---
+++ Proyecto/Proyecto_Wang/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\erick\documents\github\wang_python\proyecto\proyecto_wang\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

ad350fa7b11525ebb3db08064d41088e0ff183c0
Delia Hernández <deliaeliza72@gmail.com>
2018-10-02 22:39:49 -0600

    Delia practica

---
+++ Practicas/PracticaDelia/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\elizab~2\docume~1\github\wang_p~1\practi~1\practi~2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

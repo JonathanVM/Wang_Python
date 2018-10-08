8d5ff32fe67a925e055c6c332f0a7fdab4379f2e
Delia Hernández <deliaeliza72@gmail.com>
2018-10-08 00:01:20 -0600

    principal bien

--- Proyecto/Wang/pages/views.py
+++ Proyecto/Wang/pages/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http  import HttpResponse
+from .models import Page
 
-def index(request):
+def index(request, pagename):
     #return HttpResponse("<h1>Hola</h1>")
-    return render(request, "base.html")
+    #return render(request, "base.html")
+    #return render(request, "pages/page.html")
+    pagename = "/" + pagename
+    the_page = Page.objects.get(permalink=pagename)
+    context = {
+        "title": the_page.title,
+        "content": the_page.bodytext,
+        "last_updated": the_page.update_date
+    }
+    return render(request, "pages/page.html", context)

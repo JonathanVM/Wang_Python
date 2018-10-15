7efc2e322013e8b908e8b55ca411e97eb0ee3c7f
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2018-10-14 23:48:27 -0600

    c

--- Proyecto/Proyecto_Wang/Scripts/activate.bat
+++ Proyecto/Proyecto_Wang/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Erick\Documents\GitHub\Wang_Python\Proyecto\Proyecto_Wang"
+set "VIRTUAL_ENV=C:\Users\jonat\Documents\GitHub\Wang_Python\Proyecto\Proyecto_Wang"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"

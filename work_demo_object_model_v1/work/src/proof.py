e6c14142ce6d9459ef7f0d90b35b72b84a9e54e7
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-10-18 01:11:49 -0600

    proyecto

--- work_demo_object_model_v1/work/src/proof.py
+++ work_demo_object_model_v1/work/src/proof.py
@@ -37,7 +37,6 @@ class Rule(metaclass=ABCMeta):
 class Axiom(Rule):
     def __init__(self):
         super().__init__(RuleType.AXIOM)
-    
     def apply(self, deduction):
         for (pi, p) in enumerate(deduction.left):
             if p in deduction.right:
@@ -50,14 +49,39 @@ class AndLeft(Rule):
     def __init__(self):
         super().__init__(RuleType.AND_LEFT)
     def apply(self, deduction):
-        ands = (f for f in deduction.left if isinstance(f, And))
-        for (p, f) in enumerate(ands):
+        ands = ((p, f) for (p, f) in enumerate(deduction.left) if isinstance(f, And))
+        for (p, f) in ands:
             newleft = utils.replace(deduction.left, p, [f.left, f.right])
             yield (self.kind, p, Deduction(list(newleft), deduction.right))
             
-AND_LEFT_RULE= AndLeft()        
-            
-        
+AND_LEFT_RULE= AndLeft()
+
+class OrLeft(Rule):
+    def __init__(self):
+        super().__init__(RuleType.OR_LEFT)
+    def apply(self, deduction):
+        ors1 = ((p, f) for (p, f) in enumerate(deduction.left) if isinstance(f, Or))
+        for (p, f) in ors1:
+            newleft = utils.replace(deduction.left, p, [f.left])
+            yield (self.kind, p, Deduction(list(newleft), deduction.right))
+            newRight = utils.replace(deduction.left, p, [f.right])
+            yield (self.kind, p, Deduction(list(newRight), deduction.right))
+
+OR_LEFT_RULE= OrLeft()
+
+class AndRight(Rule):
+    def __init__(self):
+        super().__init__(RuleType.AND_RIGHT)
+    def apply(self, deduction):
+        ands1 = ((p, f) for (p, f) in enumerate(deduction.right) if isinstance(f, And))
+        for (p, f) in ands1:
+            newleft = utils.replace(deduction.right, p, [f.left])
+            yield (self.kind, p, Deduction(deduction.left, list(newleft)))
+            newRight = utils.replace(deduction.right, p, [f.right])
+            yield (self.kind, p, Deduction(deduction.left, list(newRight)))
+
+AND_RIGHT_RULE = AndRight()
+
 if __name__ == "__main__":
     print("*** Testing Proofs ***")
     t = TRUE
@@ -66,14 +90,18 @@ if __name__ == "__main__":
     q = Atom("q")
     a = And(p, q)
     na = Not(a)
-    b = Or(a, na)
+    b = Or(a, p)
     c = Then(p, b)
-    ded = Deduction([a, b, p], [na, c, p])
+    ded = Deduction([b, a, b], [c, na, a])
     print("1) Axiom test", ded)
     for f in AXIOM_RULE.apply(ded):
         print(f)
     print("2) AndLeft Test", ded)
     for f in AND_LEFT_RULE.apply(ded):
         print(f)
-    
-    
+    print("3) OrLeft Test", ded)
+    for f in OR_LEFT_RULE.apply(ded):
+       print(f)
+    print("4) AndRight Test", ded)
+    for f in AND_RIGHT_RULE.apply(ded):
+        print(f)

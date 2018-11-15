f7ba31c9d91ed30dc3bdd34fc4ac270e0bffdb0b
erickhdz01 <43705709+erickhdz01@users.noreply.github.com>
2018-11-14 22:29:20 -0600

    carpetas borradas

--- Proyecto/Proyecto Wang/Servicios/Wang/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    #url(r'^$', views.index, name='index'),
-    url(r'^consulta$', views.expresion, name='expresion'),
-    url(r'([^/]*)', views.index, name='index')
-]

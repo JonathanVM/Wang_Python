c190414f66729c8082cb18e94c71cb053f93c1c5
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-11-14 22:16:03 -0600

    carpeta aparte.

---
+++ Proyecto/Proyecto Wang/Servicios/Wang/pages/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    url(r'^consulta$', views.expresion, name='expresion'),
+    url(r'([^/]*)', views.index, name='index')
+]

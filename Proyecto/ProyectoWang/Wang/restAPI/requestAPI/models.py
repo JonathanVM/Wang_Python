fe62da4d5bbd77912596a77198fff2a09d2fdf10
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-11-18 21:07:41 -0600

    expresion, respuesta

--- Proyecto/ProyectoWang/Wang/restAPI/requestAPI/models.py
+++ Proyecto/ProyectoWang/Wang/restAPI/requestAPI/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 class Pruebas(models.Model):
     expresion = models.CharField(max_length=100)
+    respuesta = models.CharField(max_length=1500)
     def __str__(self):
         return self.expresion
--- Proyecto/ProyectoWang/Wang/restAPI/requestAPI/serializers.py
+++ Proyecto/ProyectoWang/Wang/restAPI/requestAPI/serializers.py
@@ -11,22 +11,21 @@ from deduction import *
 from deductionTree import *
 
 class PruebasSerializer(serializers.HyperlinkedModelSerializer):
-    #respuesta = serializers.Field(source='respuesta') 
     class Meta:
         model = Pruebas
-        fields = ('id','url','expresion')
+        fields = ('id','url','expresion','respuesta')
+        read_only_fields = ('respuesta',)
         
     def create(self, data):
         print(f">>>probador {data}")
         expre = data['expresion']
         print(f">>>expresion {expre}")
         result = deductionParser(expre)
-        
         for i in range(len(result)):
             arbolDeduction = DeductionTree()
             salidaArbol = arbolDeduction.buildTree(result[i])
             print(salidaArbol)
-            #data['respuesta'] = salidaArbol
+            data['respuesta'] = salidaArbol
         print(f"Posicion 0: {result[0]}")
         return super().create(data)
         

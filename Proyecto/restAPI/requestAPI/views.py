18b32527e784c48e3000866ed7305787c08704da
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-11-21 10:23:50 -0600

    base vacia

---
+++ Proyecto/restAPI/requestAPI/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from .models import Pruebas
+
+import sys
+sys.path.insert(0, '..\ANTLR\src')
+from .serializers import PruebasSerializer
+
+class PruebasAPIView(generics.ListCreateAPIView):
+    queryset = Pruebas.objects.all()
+    serializer_class = PruebasSerializer
+    
+class PruebasAPIDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pruebas.objects.all()
+    serializer_class = PruebasSerializer
+    
+class PruebasAPIAllView(generics.ListAPIView):
+    queryset = Pruebas.objects.all()
+    serializer_class = PruebasSerializer
+
+# Create your views here.

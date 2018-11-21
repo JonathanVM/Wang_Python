23a171d805f22e55888fb8eb76c2ddf40c1892d1
Elizabeth Hernandez <deliaeliza72@gmail.com>
2018-11-21 10:25:05 -0600

    esto menos

--- Proyecto/restAPI/requestAPI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestapiConfig(AppConfig):
-    name = 'requestAPI'
--- Proyecto/restAPI/requestAPI/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from .views import PruebasAPIView, PruebasAPIDetail, PruebasAPIAllView
-
-urlpatterns = [
-    path('', PruebasAPIView.as_view()),
-    path('<int:pk>/',PruebasAPIDetail.as_view()),
-    path('all/',PruebasAPIAllView.as_view()),
-]

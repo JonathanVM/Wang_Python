3266917651d5c327200536fa65a29ec7cb8f1a5e
JonathanVM <33185089+JonathanVM@users.noreply.github.com>
2018-12-14 22:03:43 -0600

    Ultima version
    Probador de expreciones logicas wang, con arbol de prueba.

--- Practicas/PracticaJonathan/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jonat\documents\github\wang_python\practicas\practicajonathan\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
